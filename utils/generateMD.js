328b6674653dd09aa57a0bb0c8b3e08f0d4e8502
Hail Choi <julianchoiii11@gmail.com>
2022-08-12 14:35:39 -0700

    worked on md generate function

--- utils/generateMD.js
+++ utils/generateMD.js
@@ -35,3 +35,30 @@ else {
 * Here is the link detailing license terms and conditions ${renderLicenseLink(license)}`;
 }
 };
+
+const generateMarkdown = data => {
+  return `# ${data.pname} ![Github license](${renderLicenseBadge(data.plicense)})
+  ## Description
+  ${data.pdescription}
+  ## Table of Contents:
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributor](#Contributors)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  ## Installation:
+  Install dependency using command : ${data.pdependency}
+  ## Usage:
+  ${data.pusage}
+  ## License:
+  ${renderLicenseSection(data.plicense)}
+  ## Contributors:
+  ${data.pcontributor}
+  ## Tests:
+  ${data.ptest}
+  ## Questions:
+  * Link to my GitHub profile : https://github.com/${data.username}  
+  * For additional questions reach me at : ${data.eaddress}
+  `;
+  }
